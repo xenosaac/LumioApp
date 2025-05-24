9cd5c2f6d702c224b09b6c4e76017a5edd44b2ed
Pranav Gonuguntla <spgonugu@uci.edu>
2025-05-24 12:01:57 -0700

    dashboard

--- RA/Views/DreamTrackerView.swift
+++ RA/Views/DreamTrackerView.swift
@@ -1,22 +1,36 @@
+//import SwiftUI
+//
+//struct DreamTrackerView: View {
+//    var body: some View {
+//        NavigationView {
+//            VStack {
+//                Text("Under Construction")
+//                    .font(.largeTitle)
+//                    .fontWeight(.bold)
+//                    .foregroundColor(.gray)
+//                
+//                Spacer().frame(height: 20)
+//                
+//                Image(systemName: "hammer.fill")
+//                    .font(.system(size: 60))
+//                    .foregroundColor(.gray)
+//                    .padding()
+//            }
+//        }
+//    }
+//}
+//
+//struct DreamTrackerView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        DreamTrackerView()
+//    }
+//}
+
 import SwiftUI
 
 struct DreamTrackerView: View {
     var body: some View {
-        NavigationView {
-            VStack {
-                Text("Under Construction")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .foregroundColor(.gray)
-                
-                Spacer().frame(height: 20)
-                
-                Image(systemName: "hammer.fill")
-                    .font(.system(size: 60))
-                    .foregroundColor(.gray)
-                    .padding()
-            }
-        }
+        DreamDashboardView()
     }
 }
 
@@ -24,4 +38,4 @@ struct DreamTrackerView_Previews: PreviewProvider {
     static var previews: some View {
         DreamTrackerView()
     }
-} 
+}
